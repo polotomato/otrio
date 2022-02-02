d1d24a9de2ea8f87cbdd79abec8c307f4c7e8dac
polotomato <aotomato@gmail.com>
2022-02-02 19:12:05 +0900

    アソシエーション、メッセージの文字制限

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+  validates :content, presence: true,
+                      length: { minimum: 1, maximum: 200 }
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true, length: { maximum: 50 }
+
+  has_many :messages
 end
