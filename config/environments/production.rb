408c8f25846ba3f9b300b524c46782c6c52af44e
polotomato <aotomato@gmail.com>
2022-02-04 12:13:36 +0900

    自分ドメインからのリクエストを許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://otrio.net/cable'
+  config.action_cable.allowed_request_origins = [ 'https://otrio.net' ]
+  ActionCable.server.config.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
