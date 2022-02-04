9b2e735c6fd849e0001bedad042c7347270ef715
polotomato <aotomato@gmail.com>
2022-02-04 19:06:10 +0900

    部屋ごとのチャット機能を実装
    部屋ごとのチャットは保存しない

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,15 +1,16 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel_#{params['room']}"
-    ActionCable.server.broadcast "room_channel_#{params['room']}", message: render_message("#{current_user.nickname}さんが入室しました")
+    RoomMessageBroadcastJob.perform_later({ room_id: params['room'], msg: "#{current_user.nickname}さんが入室しました" })
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    ActionCable.server.broadcast "room_channel_#{params['room']}", message: render_message("#{current_user.nickname}さんが退室しました")
+    RoomMessageBroadcastJob.perform_later({ room_id: params['room'], msg: "#{current_user.nickname}さんが退室しました" })
   end
 
   def speak(data)
+    RoomMessageBroadcastJob.perform_later({ msg: data['message'], room_id: params['room'], user_nickname: current_user.nickname })
   end
 
   private
---
+++ app/jobs/room_message_broadcast_job.rb
@@ -0,0 +1,16 @@
+class RoomMessageBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(data)
+    ActionCable.server.broadcast "room_channel_#{data[:room_id]}", message: render_message({msg: data[:msg], user_nickname: data[:user_nickname]})
+  end
+
+  private
+
+  def render_message(data)
+    ApplicationController.renderer.render(partial: 'messages/message',
+                                          locals: { message: data[:msg],
+                                                    user_nickname: data[:user_nickname]
+                                          })
+  end
+end
