ef36e351a3419b06cfeeffddd415b87622d6ef84
polotomato <aotomato@gmail.com>
2022-02-04 17:22:08 +0900

    ヘッダーを部分テンプレート化・入室退室アラート

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,13 +1,20 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel_#{params['room']}"
+    ActionCable.server.broadcast "room_channel_#{params['room']}", message: render_message("#{current_user.nickname}さんが入室しました")
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    ActionCable.server.broadcast "room_channel_#{params['room']}", message: render_message("#{current_user.nickname}さんが退室しました")
   end
 
   def speak(data)
-    Message.create!(content: data['message'], user_id: current_user.id)
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.renderer.render partial: 'messages/message', locals: { message: message, user_nickname: nil }
   end
 end
