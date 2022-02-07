11db433ed3cbc0048e706898caf91156e5ab64f8
polotomato <aotomato@gmail.com>
2022-02-07 17:13:08 +0900

    部屋の参加者のリアルタイムでの追加と削除

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,18 +2,18 @@ class RoomChannel < ApplicationCable::Channel
   def subscribed
     stop_all_streams
     stream_from "room_channel_#{params['room']}"
-    RoomMessageBroadcastJob.perform_later({ room_id: params['room'], msg: "#{current_user.nickname}さんが入室しました" })
+    RoomMessageBroadcastJob.perform_later({ status: "user-in", room_id: params['room'], user_id: current_user.id, nickname: current_user.nickname, msg: "#{current_user.nickname}さんが入室しました" })
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     records = RoomUser.where("user_id = ?", current_user.id)
     records.destroy_all
-    RoomMessageBroadcastJob.perform_later({ room_id: params['room'], msg: "#{current_user.nickname}さんが退室しました" })
+    RoomMessageBroadcastJob.perform_later({ status: "user-out", room_id: params['room'], user_id: current_user.id, nickname: current_user.nickname, msg: "#{current_user.nickname}さんが退室しました" })
   end
 
   def speak(data)
-    RoomMessageBroadcastJob.perform_later({ msg: data['message'], room_id: params['room'], user_nickname: current_user.nickname })
+    RoomMessageBroadcastJob.perform_later({ status: "user-chat", msg: data['message'], room_id: params['room'], user_nickname: current_user.nickname })
   end
 
   private
