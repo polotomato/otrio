62c3286745cc05ceab48a257d34c042d2665248f
polotomato <aotomato@gmail.com>
2022-02-08 19:22:08 +0900

    バリデーション・アソシエーション

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -1,2 +1,10 @@
 class GamePlayer < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+
+  validates :seat, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 4
+  }
 end
