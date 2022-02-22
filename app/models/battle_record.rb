0f7e0ef26aafdf7f466789af68f15fd6a64db973
polotomato <aotomato@gmail.com>
2022-02-22 11:56:35 +0900

    ユーザー詳細画面を実装

--- app/models/battle_record.rb
+++ app/models/battle_record.rb
@@ -1,3 +1,7 @@
 class BattleRecord < ApplicationRecord
   belongs_to :winner, class_name: 'User', foreign_key: :winner_id
+  belongs_to :red,    class_name: 'User', foreign_key: :red_id
+  belongs_to :green,  class_name: 'User', foreign_key: :green_id
+  belongs_to :purple, class_name: 'User', foreign_key: :purple_id
+  belongs_to :blue,   class_name: 'User', foreign_key: :blue_id
 end
